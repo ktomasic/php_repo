c669b479cd3b3559280b5f3c0acf3750ca42df20
ktomasic <karlo.tomasic@outlook.com>
2017-07-28 16:37:16 +0200

    commit

--- php_srce/practice2.1/index.php
+++ php_srce/practice2.1/index.php
@@ -7,8 +7,19 @@
 
    <body>
       <?php
-         $a=10;
-         echo $a;
+         $a = 10;
+         //echo $a;
+         //echo "<br/>";
+         $b = 15;
+         //echo $b;
+         $c = (3 * $a - $b) / 2;
+         //echo "<hr/>";
+         //echo $c;
+         $tekst1 = "a = " . $a;
+         $tekst2 = "b = " . $b;
+         $tekst3 = "c = " . $c;
+
+         echo $tekst1 . "<br/>" . $tekst2 . "<hr/>" . $tekst3;
       ?>
    </body>
 
