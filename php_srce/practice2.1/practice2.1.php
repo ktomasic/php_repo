0a26664ddbee371b70a52d45cc0b5c9c2c66280e
ktomasic <karlo.tomasic@outlook.com>
2017-07-28 15:37:05 +0200

    commit

---
+++ php_srce/practice2.1/practice2.1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+   <head>
+     <meta charset="utf-8">
+     <title>Moja Prva Stranica</title>
+   </head>
+
+   <body>
+      <?php
+         $a=10;
+         echo $a;
+      ?>
+   </body>
+
+</html>
